9f5ffe4bf9e323996aa347428f7f3c35ce099d00
VaheMyan <130179116+VaheMyan@users.noreply.github.com>
2024-04-06 15:34:34 +0400

    ANIM FINISH Yet7

--- My project (8)/Assets/Scripts/Animacion/DogAnimSystem.cs
+++ My project (8)/Assets/Scripts/Animacion/DogAnimSystem.cs
@@ -1,12 +1,12 @@
 using System;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 public class DogAnimSystem : ComponentSystem
 {
     private EntityQuery _query;
-    public int Test;
     protected override void OnCreate()
     {
         _query = GetEntityQuery(ComponentType.ReadOnly<MoveAnimData>(), ComponentType.ReadOnly<Animator>(), ComponentType.ReadOnly<AttackAnimData>(),
@@ -19,12 +19,23 @@ public class DogAnimSystem : ComponentSystem
         {
             animator.SetBool(inputData.moveAnimHash, Math.Abs(move.Move.x) > 0.05f || Math.Abs(move.Move.y) > 0.05f); // Walk anim
             animator.SetBool(inputData.attackAnimHash, Math.Abs(move.Shoot) > 0f); // Attack aim
-            Debug.Log(animator.GetBool(inputData.moveAnimHash));
             animator.SetBool(inputData.getHitAnimHash, Math.Abs(move.CollideInput) > 1.5f && characterHealth._health > 0); // GetHit anim
             animator.SetBool(inputData.dieAnimHash, characterHealth.isDie);
 
             if (inputData.moveAnimSpeedHash == String.Empty) return;
-            animator.SetFloat(inputData.moveAnimSpeedHash, inputData.speed * math.distancesq(move.Move.x, move.Move.y));
+            if (-move.Move.x == move.Move.y)
+            {
+                animator.SetFloat(inputData.moveAnimSpeedHash, inputData.speed * math.distancesq(move.Move.x, move.Move.y));
+            }
+            if (move.Move.x == move.Move.y)
+            {
+                animator.SetFloat(inputData.moveAnimSpeedHash, inputData.speed * math.distancesq(-move.Move.x, move.Move.y));
+            }
+            else
+            {
+                animator.SetFloat(inputData.moveAnimSpeedHash, inputData.speed * math.distancesq(move.Move.x, move.Move.y));
+            }
+
         });
     }
 }
